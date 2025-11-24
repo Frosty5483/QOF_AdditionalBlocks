65e1f579a924a16fa83e546e9cfbdc4ff9c203dd
GNUTButtah <gabrielgerlitz@gmail.com>
2025-11-24 17:22:48 +0100

    I did something!

--- src/main/java/com/testmod/TestMod.java
+++ src/main/java/com/testmod/TestMod.java
@@ -9,6 +9,8 @@ public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+//HELLO I AM GGBUTTAH
+
 	@Override
 	public void onInitialize() {
 
